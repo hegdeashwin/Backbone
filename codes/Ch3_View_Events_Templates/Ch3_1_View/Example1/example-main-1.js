886488dad4905accaa653da7c975f01a1dd22395
hegdeashwin <ashwin.hegde3@gmail.com>
2015-09-25 23:49:27 +0530

    Updates egs

--- codes/Ch3_View_Events_Templates/Ch3_1_View/Example1/example-main-1.js
+++ codes/Ch3_View_Events_Templates/Ch3_1_View/Example1/example-main-1.js
@@ -1,89 +1,78 @@
 (function() {
-	/*
-		The goal of this file is to provide the basic understanding
-		1. Create a View class.
-		2. Reference with DOM using tabName, className, id.
 
-		How to run this example.
-		1. Open Example-1.html in Google Chrome browser.
-		2. Press F12, go to console tab.
-		3. See the message get displayed on that console tab.
-	*/
+	/**
+	 * The goal of this file is to provide the basic understanding of
+	 * 1. Create Backbone View
+	 * 2. Create instance of Backbone View
+	 * 3. Setting default el reference
+	 * 4. Setting classname, id and tagname
+	 * 5. Setting custom el reference
+	 */
 
-	/*
-		Creating a new View called MasterView by extending Backbone.View class.
-		Syntax: Backbone.View.extend(properties, [classProperties])
-	*/
+	/**
+	 * Creating a new View called MasterView by extending Backbone.View class
+	 * Syntax: Backbone.View.extend(properties, [classProperties])
+	 */
 	var MasterView = Backbone.View.extend({
-		/*
-			This initialize function will get called when the View is first created.
-		*/
+		/**
+		 * `initialize` is a constructor function which gets called automatically at
+		 * the time of instance creation
+		 */
 		initialize: function() {
 			console.log("Your View have been initialize.");
 		},
 
-		/*
-			Optional property, this property will let you set tagName's class name.
-			You can also set multiple classes like so 'container mainpage centerPanel ...'
-		*/
+		/**
+		 * if `tagName` is not specified then its default to `div`;
+		 * This will set class name for the DOM element to which your view is refering to
+		 * You can also set multiple classes like 'container mainpage centerPanel ...'
+		 * This is optional property;
+		 */
 		className: 'container',
 
-		/*
-			Optional property, this property will let you set tagName's id.
-		*/
+		/**
+		 * This will set id for the DOM element to which your view is refering to
+		 * This is optional property;
+		 */
 		id: 'master'
 
 	});
 
-	/*
-		Creating an object from MasterView which calls initialize function.
-	*/
+	/**
+	 * Creating an instance of MasterView.
+	 */
 	var masterView = new MasterView();
 	console.log(masterView);
 
-	/*
-		In master view, tabName is not set. So, <div> is the default.
-		masterView.el will display <div id="master" class="container"></div>
-	*/
-	console.log("'el' from master view: Here tabName is not set. So, <div> is the default.");
+	/**
+	 * masterView.el will display <div id="master" class="container"></div>
+	 */
 	console.log(masterView.el);
 
-
-	/*
-		Creating a new view called ChildView by extending Backbone.View class.
-	*/
+	/**
+	 * Creating a new view called ChildView by extending Backbone.View class.
+	 */
 	var ChildView = Backbone.View.extend({
 
-		/*
-		 	Required property, if not set Backbone will set the defaults to 'div'.
-		 	Here, tagName is specified as 'ul'.
-		*/
+		/**
+		 * tagName is specified as 'ul' will override the default
+		 */
 		tagName: 'ul',
 
-		/*
-			Optional property, this property will let you set tagName's class name.
-			You can also set multiple classes like so 'container mainpage centerPanel ...'
-		*/
-		className: 'container',
+		className: 'row',
 
-		/*
-			Optional property, this property will let you set tagName's id.
-		*/
 		id: 'child'
 
 	});
 
-	/*
-		Creating an object from ChildView.
-	*/
+	/**
+	 * Creating an instance of ChildView.
+	 */
 	var childView = new ChildView();
 
-	/*
-		In child view, tabName is set to 'ul'. Thus override the default value for 'el'.
-		childView.el will display <ul id="child" class="container"></ul>
-	*/
-	console.log("'el' from child view: Here tabName is set to 'ul'. Thus override the default value for 'el'.");
+	/**
+	 * childView.el will display <div id="child" class="row"></div>
+	 */
 	console.log(childView.el);
-
 
 })();
